e6c393a946666e2aa37c9284b4995f0b95f62d23
Mahammad <mahammad.rustamov.2001@gmail.com>
2023-11-06 17:51:13 +0400

    Question page created,parse html created and view question code block finished

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -1,4 +1,9 @@
+import Answer from "@/components/forms/Answer";
+import Metric from "@/components/shared/Metric";
+import ParseHTML from "@/components/shared/ParseHTML";
+import RenderTag from "@/components/shared/RenderTag";
 import { getQuestionById } from "@/lib/actions/question.action";
+import { getTimestamp, formatAndDivideNumber } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -27,7 +32,50 @@ const Page = async ({ params, searchParams }) => {
 
           <div className="flex justify-end ">VOTING</div>
         </div>
+
+        <h2 className="h2-semibold text-dark200_light900 mt-3.5 w-full text-left">
+          {result.title}
+        </h2>
       </div>
+
+      <div className="mb-8 mt-5 flex flex-wrap gap-4">
+        <Metric
+          imgUrl="/assets/icons/clock.svg"
+          alt="clock icon"
+          value={` asked ${getTimestamp(result.createdAt)}`}
+          title=" "
+          textStyles="small-medium text-dark400_light800"
+        />
+        <Metric
+          imgUrl="/assets/icons/message.svg"
+          alt="message"
+          value={formatAndDivideNumber(result.answers.length)}
+          title=" Answers"
+          textStyles="small-medium text-dark400_light800"
+        />
+        <Metric
+          imgUrl="/assets/icons/eye.svg"
+          alt="eye"
+          value={formatAndDivideNumber(result.views)}
+          title=" Views"
+          textStyles="small-medium text-dark400_light800"
+        />
+      </div>
+
+      <ParseHTML data={result.content} />
+
+      <div className="mt-8 flex flex-wrap gap-2">
+        {result.tags.map((tag: any) => (
+          <RenderTag
+            key={tag._id}
+            _id={tag._id}
+            name={tag.name}
+            showCount={false}
+          />
+        ))}
+      </div>
+
+      <Answer />
     </>
   );
 };
